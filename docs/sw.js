b6b3771bdb0368aff3eafd3193fcd25222568fd5
Scott Antwi <scottantwi930@gmail.com>
2026-02-20 12:33:57 +0000

    fix pwa icons and caching

--- docs/sw.js
+++ docs/sw.js
@@ -1,8 +1,12 @@
-const CACHE_NAME = 'malaria-detector-v1';
+const CACHE_NAME = 'malaria-detector-v2';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './model/model.json',
+  './model/group1-shard1of1.bin',
   'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.17.0/dist/tf.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
   'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap'
